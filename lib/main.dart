871e15823e11442eccba6a31a053e8b9e7d466e9
Bigg PAPA <nito03@icloud.com>
2026-03-18 14:28:49 -0400

    Added a habit building alert box, the next step is to store information and build habit pages because of it

--- lib/main.dart
+++ lib/main.dart
@@ -10,20 +10,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Pages(),
+      home: Habits(),
     );
   }
 }
 
-class Pages extends StatefulWidget {
-  const Pages({super.key});
+class Habits extends StatefulWidget {
+  const Habits({super.key});
 
   @override
-  State<Pages> createState() => _Pages();
+  State<Habits> createState() => _HabitsState();
 }
 
-class _Pages extends State<Pages> {
+class _HabitsState extends State<Habits> {
   int selectedIndex = 0;
+  List<String> habits = [];
 
   Widget _buildPage() {
     switch (selectedIndex) {
@@ -47,7 +48,101 @@ class _Pages extends State<Pages> {
     }
   }
 
-  void changePage(int index) {
+void addHabit() {
+  final formKey = GlobalKey<FormState>();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController timeController = TextEditingController();
+  final TextEditingController frequencyController = TextEditingController();
+
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Creating a new habit'),
+        content: SingleChildScrollView(
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  controller: nameController,
+                  decoration: const InputDecoration(
+                    labelText: "Habit Name",
+                    hintText: "Example: Laundry",
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please enter a habit name';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 12),
+                TextFormField(
+                  controller: frequencyController,
+                  decoration: const InputDecoration(
+                    labelText: "Frequency",
+                    hintText: "1(once a day), 2(twice a day)",
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please enter a frequency';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 12),
+                TextFormField(
+                  controller: timeController,
+                  maxLines: 1,
+                  decoration: const InputDecoration(
+                    labelText: "What time do you want the app to hold you to",
+                    hintText: "0130 (hrs mins)",
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please enter a time';
+                    }
+                    return null;
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Icon(Icons.cancel, size: 20),
+          ),
+          TextButton(
+            onPressed: () {
+              if (formKey.currentState!.validate()) {
+                setState(() {
+                  habits.add(
+                    '${nameController.text.trim()} | '
+                    '${frequencyController.text.trim()} | '
+                    '${timeController.text.trim()}',
+                  );
+                });
+                Navigator.pop(context);
+              }
+            },
+            child: const Icon(Icons.check_box, size: 25),
+          ),
+        ],
+      );
+    },
+  );
+}
+
+  void changePages(int index) {
     setState(() {
       selectedIndex = index;
     });
@@ -61,33 +156,48 @@ class _Pages extends State<Pages> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
 
             _buildPage(),
 
-            const SizedBox(height: 30),
+            const SizedBox(height: 10),
 
-            Column(
+            Row(
+              
               mainAxisAlignment: MainAxisAlignment.center,
+              
               children: [
                 ElevatedButton(
-                  onPressed: () => changePage(0),
+                  onPressed: () => changePages(0),
                   child: const Text('Home'),
                 ),
                 const SizedBox(width: 15),
                 ElevatedButton(
-                  onPressed: () => changePage(1),
+                  onPressed: () => changePages(1),
                   child: const Text('1'),
                 ),
                 const SizedBox(width: 15),
                 ElevatedButton(
-                  onPressed: () => changePage(2),
+                  onPressed: () => changePages(2),
                   child: const Text('2'),
                 ),
               ],
             ),
+            const SizedBox(height: 10),
+            Column(children: [
+              const SizedBox(width: 25),
+              SizedBox(
+                width: double.infinity,
+                height: 60,
+                child:
+                ElevatedButton(onPressed: addHabit,
+                child: const Icon(Icons.add, size:35)
+                ),
+              ),
+            ],)
           ],
+        
         ),
       ),
     );
