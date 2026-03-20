7c38be4f226adc4039ee4e99d0f84446f38d9cf2
Hien Dao <hienqdao5@gmail.com>
2026-03-20 17:50:06 -0400

    Remove streak and change repeat type to enum in habit.dart

--- lib/data/models/habit.dart
+++ lib/data/models/habit.dart
@@ -1,24 +1,17 @@
+import '../../core/constants/enums.dart';
+
 class Habit {
   final int? id;
   final String name;
   final String? description;
 
-  // Repeat type: weekly, monthly, yearly
-  final String repeatType;
+  final RepeatType repeatType;
 
-  // Weekly
-  final int? dayOfWeek;     // 0 = Monday, 6 = Sunday
+  final int? dayOfWeek;
+  final int? dayOfMonth;
+  final int? month;
 
-  // Monthly / Yearly
-  final int? dayOfMonth;    // 1–31
-
-  // Yearly only
-  final int? month;         // 1–12
-
-  // Time of day
-  final String timeOfDay;   // "17:00"
-
-  final int streak;
+  final String timeOfDay;
   final String createdAt;
 
   Habit({
@@ -30,7 +23,6 @@ class Habit {
     this.dayOfMonth,
     this.month,
     required this.timeOfDay,
-    this.streak = 0,
     required this.createdAt,
   });
 
@@ -39,12 +31,11 @@ class Habit {
       'id': id,
       'name': name,
       'description': description,
-      'repeat_type': repeatType,
+      'repeat_type': repeatType.name,
       'day_of_week': dayOfWeek,
       'day_of_month': dayOfMonth,
       'month': month,
       'time_of_day': timeOfDay,
-      'streak': streak,
       'created_at': createdAt,
     };
   }
@@ -54,12 +45,13 @@ class Habit {
       id: map['id'],
       name: map['name'],
       description: map['description'],
-      repeatType: map['repeat_type'],
+      repeatType: RepeatType.values.firstWhere(
+        (e) => e.name == map['repeat_type'],
+      ),
       dayOfWeek: map['day_of_week'],
       dayOfMonth: map['day_of_month'],
       month: map['month'],
       timeOfDay: map['time_of_day'],
-      streak: map['streak'],
       createdAt: map['created_at'],
     );
   }
