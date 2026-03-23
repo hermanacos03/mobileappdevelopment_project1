fe266d8d4f31581e2e08a0fb51397e2f37e66430
Bigg PAPA <nito03@icloud.com>
2026-03-23 09:52:48 -0400

    Stylitic changes

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,39 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PageControllerapp(),
+
+
+      themeMode: ThemeMode.dark,
+
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+
+
+        scaffoldBackgroundColor: Colors.black,
+
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 128, 109, 14),
+          foregroundColor: Colors.white,
+          elevation: 0,
+        ),
+
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.grey[900],
+            foregroundColor: Colors.white,
+          ),
+        ),
+
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.white),
+          titleLarge: TextStyle(color: Colors.white),
+        ),
+      ),
+
+      home: const PageControllerapp(),
     );
   }
 }
